7b8c7f8cf95d2a4f8131dff8c40b50e6b43579ab
Myron <dubtwee@gmail.com>
2020-07-02 10:26:12 -0500

    adding notes for App requirements

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,11 @@ $(document).ready(handleReady);
 
 function handleReady() {
   console.log("jquery is loaded!")
+  // TODO: listen for submit of form
 }
+
+// TODO: handle form submit
+
+// TODO: send submitted guess to server
+
+// TODO: get all guesses history from server
--- server/server.js
+++ server/server.js
@@ -9,8 +9,15 @@ app.use(bodyParser.urlencoded({extended:true}))
 // Serve up static files (HTML, CSS, Client JS)
 app.use(express.static('server/public'));
 
+// TODO: function that generate rando number
+
 // GET & POST Routes go here
 
+// TODO: GET for all guesses history
+
+// TODO: POST to receive my round of guesses
+
+// TODO: POST to reset rando number
 
 app.listen(PORT, () => {
   console.log ('Server is running on port', PORT)
