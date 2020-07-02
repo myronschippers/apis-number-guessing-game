15d48247b7d770cbf73ae3010e38e60dd6cdd7e6
Myron <dubtwee@gmail.com>
2020-07-02 11:38:49 -0500

    collecting guesses and organizing into array

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,9 +3,32 @@ $(document).ready(handleReady);
 function handleReady() {
   console.log("jquery is loaded!")
   // TODO: listen for submit of form
+  $('#jsGuessForm').on('submit', submitGuesses);
 }
 
 // TODO: handle form submit
+function submitGuesses(event) {
+  event.preventDefault();
+  const playerGuesses = [
+    {
+      name: 'Myron',
+      guess: $('.js-myron-guess').val(),
+    },
+    {
+      name: 'Ryan',
+      guess: $('.js-ryan-guess').val(),
+    },
+    {
+      name: 'Shelby',
+      guess: $('.js-shelby-guess').val(),
+    },
+    {
+      name: 'Ashleigh',
+      guess: $('.js-ashleigh-guess').val(),
+    }
+  ];
+  console.log('GUESSed:', playerGuesses);
+}
 
 // TODO: send submitted guess to server
 
